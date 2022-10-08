2827204671024f674e9c70bdfd63edbdce965225
Rob Thorne <rob@torenware.com>
2022-10-08 13:56:11 -0700

    Remove config parser func

--- viteserve/build-dotenv.sh
+++ viteserve/build-dotenv.sh
@@ -6,24 +6,6 @@ print_usage() {
   printf "Usage: $0 [-y]"
 }
 
-# https://stackoverflow.com/a/21189044
-function parse_yaml {
-  local prefix=$2
-  local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @ | tr @ '\034')
-  sed -ne "s|^\($s\):|\1|" \
-    -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-    -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" $1 |
-    awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-   }'
-}
-
 while getopts 'y' flag; do
   case "${flag}" in
   y) y_flag='true' ;;
@@ -33,10 +15,6 @@ while getopts 'y' flag; do
     ;;
   esac
 done
-
-# if [ -f .ddev/config.yaml ]; then
-#   eval $(parse_yaml .ddev/config.yaml)
-# fi
 
 # defaults
 PROJ_DIR=frontend
